3cce0c75d653ac50719a02d065c6c81fa0df0aca
bharath <bharathofficial133@gmail.com>
2026-08-11 22:38:29 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Loop{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int i=sc.nextInt();
+        while(i<=5){
+            System.out.println(i);
+            i++;
+
+        }
+    }
+}
