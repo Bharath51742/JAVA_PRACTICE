9251e01170f1f79cad28cdbcef34af6023d88e74
bharath <bharathofficial133@gmail.com>
2026-08-11 22:32:24 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Swap {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+
+    System.out.println("Enter a number:");
+    int a = sc.nextInt();
+
+    System.out.println("Enter b number:");
+    int b = sc.nextInt();
+
+    int temp = a;
+    a = b;
+    b = temp;
+
+    System.out.println("a = " + a);
+    System.out.println("b = " + b);
+}
+
+
+}
