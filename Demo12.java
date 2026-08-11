90bb7a80ee98e8d504b49c1befffec3a6af38648
bharath <bharathofficial133@gmail.com>
2026-08-11 22:36:48 +0530

    Add files via upload

---
+++ Demo12.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Demo12{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the name");
+         String name=sc.nextLine();
+        
+        System.out.println(" hello " +name);
+        sc.close();
+    }
+    
+}
+
